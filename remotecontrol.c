fa82e3f1aec45bd62ecdff1c8ecce3a443a32a42
Chris <crpiatka@hotmail.de>
2015-05-20 10:41:12 +0200

    aall

--- remotecontrol.c
+++ remotecontrol.c
@@ -3,3 +3,30 @@
 #include "usart.h"
 #include <stdio.h>
 #include <stdlib.h>
+
+RemoteSignal getRemoteSingnal(){ 
+
+	uint8_t data [1]; 
+
+	read_values(17, data[], 1); 
+	uint8_t rem = data[0];
+
+	if (rem == FORWARD){
+		return ACCELERATE; 
+	}
+	else if (rem == BACKWARDS){
+		return BRAKE;
+	}
+	else if (rem == LEFT){
+		return RLEFT; 
+		}
+	else if (rem == RIGHT){
+		return RRIGHT; 
+		}
+	else if (rem == LEFT){
+		return RLEFT; 
+		}
+	else {
+		return RNOTHINGPRESSED; 
+		}
+	}
